95e5b2a3e5866e613866207ce79664ff18c93dd0
jf.giammari <giammari.jf@gmail.com>
2020-09-07 23:33:04 +0200

    Update Boolean

--- ATELIER 1/PARTIE 1/Exercice_3.py
+++ ATELIER 1/PARTIE 1/Exercice_3.py
@@ -23,17 +23,17 @@ def test_unitaire():
     sexe_test = ["H","F","F","F","H"]
     age_test = [20,17,36,19,21]
     resultat_test= ["Vous n'êtes pas imposable","Vous n'êtes pas imposable","Vous n'êtes pas imposable","Vous êtes imposable","Vous êtes imposable"]
-    succes = 0
-    
+    succes = False
+
     for i in range(0,len(sexe_test)) :
         if imposable(sexe_test[i],age_test[i]) == resultat_test[i] :
             print(">",end="")
         else :
             print("X",end="")
-            succes = 1
+            succes = True
         time.sleep(0.1)
 
-    if succes == 0 :
+    if succes == True :
         print(" TEST IS SUCCES")
     else :
         print(" TEST IS FAILED")
