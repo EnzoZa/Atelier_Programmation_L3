17bb99a426e51bc9d5e1317f729ab74f94fc0c16
jf.giammari <giammari.jf@gmail.com>
2020-09-08 08:42:42 +0200

    Add Ex7 & Ex8

---
+++ ATELIER 1/PARTIE 2/Exercice_7.py
@@ -0,0 +1,14 @@
+#Auteur : Jean-François Giammari & Axel Frau
+#Date : 08/09/2020 
+#Version : 1
+#Description : Elections législatives
+
+#EXERCICE 7
+
+def X():
+    """ 
+    
+
+    """
+
+
---
+++ ATELIER 1/PARTIE 2/Exercice_8.py
@@ -0,0 +1,14 @@
+#Auteur : Jean-François Giammari & Axel Frau
+#Date : 08/09/2020 
+#Version : 1
+#Description : Compagnie d'assurance automobile
+
+#EXERCICE 8
+
+def tarification():
+    """ 
+    
+
+    """
+
+
